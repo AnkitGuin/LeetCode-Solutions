f1242748aafbd0a822a9de414207e20d17f7b7b9
AnkitGuin <ankitguin006@gmail.com>
2026-08-26 16:05:29 +0530

    Time: 3 ms (26.7%), Space: 8.6 MB (82.87%) - LeetHub

--- 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.cpp
+++ 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.cpp
@@ -2,22 +2,23 @@ class Solution {
 public:
     string shortestBeautifulSubstring(string s, int k) {
         int n=s.size();
-        vector<int> pref(n,0);
-        pref[0]=(s[0]=='1');
-        for(int i=1; i<n; i++) pref[i]=pref[i-1]+(s[i]=='1');
+        int ones=0;
         string ans="";
         int mx=n+1;
-        for(int i=0; i<n; i++){
-            for(int j=i; j<n; j++){
+        int i=0;
+        for(int j=0; j<n; j++){
+            if(s[j]=='1') ones++;
+            while(ones>k){
+                if(s[i]=='1') ones--;
+                i++;
+            }
+            if(ones==k){
+                while(s[i]=='0') i++;
                 int len=j-i+1;
                 string x=s.substr(i,len);
-                int ones=pref[j];
-                if(i>0) ones-=pref[i-1];
-                if(ones==k){
-                    if((len<mx) || (len==mx && x<ans)){
-                        ans=x;
-                        mx=len;
-                    }
+                if(len<mx || (len==mx && x<ans)){
+                    mx=len;
+                    ans=x;
                 }
             }
         }
